f6b70a098a5c8311d834160223eb81c83d40b0fa
KeleiAzz <kgong@ncsu.edu>
2015-11-18 18:34:57 +0000

    update localhost

--- src/www/statusModel.js
+++ src/www/statusModel.js
@@ -90,7 +90,7 @@ $(document).ready( function()
     ko.applyBindings(viewModel);
     $('#statusTable').DataTable( { "paging":   false, "info":     false });
 
-    var socket = io.connect('http://localhost:3000');
+    var socket = io.connect('http://127.0.0.1:3000');
 
     socket.on("heartbeat", function(client) 
     {
